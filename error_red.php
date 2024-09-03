65c7229471d7481d91bf8d680e46ac2d482688a2
victorberdugo <victorberdugo518@proton.me>
2024-09-03 09:50:30 -0500

    Add files via upload

---
+++ error_red.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Error al enviar pago</title>
+</head>
+<body>
+  <h1>Error al enviar pago</h1>
+  <p>Lo sentimos, pero hubo un error al enviar su pago debido a una congestión en la red. Por favor, inténtelo de nuevo más tarde.</p>
+</body>
+</html>
